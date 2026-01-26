ff771b1309e599036ddec9e4318b6ffd376913ca
Mugilan-18 <mugipp2005@gmail.com>
2026-01-26 14:13:38 +0530

    Simplify data fetching in dashboard.js
    Removed unnecessary checks for data and simplified fetch handling.

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,20 +1,11 @@
-setInterval(() => {
+setInterval(()=>{
   fetch("/api/data")
-    .then(r => r.json())
-    .then(d => {
-      if (!d) return;
-
-      if (!d.bin_id) {
-        document.getElementById("status").innerText =
-          "No ESP Data received yet";
-        return;
-      }
-
-      document.getElementById("bin_id").innerText = d.bin_id;
-      document.getElementById("area").innerText = d.area;
-      document.getElementById("gas").innerText = d.gas;
-      document.getElementById("level").innerText = d.level;
-      document.getElementById("status").innerText = d.status;
-    });
-}, 2000);
-
+  .then(res=>res.json())
+  .then(d=>{
+    document.getElementById("bin_id").innerText = d.bin_id;
+    document.getElementById("area").innerText = d.area;
+    document.getElementById("gas").innerText = d.gas;
+    document.getElementById("level").innerText = d.level;
+    document.getElementById("status").innerText = d.status;
+  });
+},2000);
