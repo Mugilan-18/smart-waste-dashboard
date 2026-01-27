306c5638ee411ca46fd50ea888200316fa43e5fa
Mugilan-18 <mugipp2005@gmail.com>
2026-01-27 21:24:46 +0530

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,20 +4,33 @@ function fetchData() {
     fetch("/api/data")
         .then(res => res.json())
         .then(res => {
+
+            const system = document.getElementById("system");
+            const totalBins = document.getElementById("totalBins");
+            const table = document.getElementById("tableBody");
+
             if (!res.connected) {
-                showNoData();
+                system.innerText = "Connection Lost";
+                system.className = "card red";
+                totalBins.innerText = "Total Bins: 0";
+                table.innerHTML =
+                    `<tr class="no-data">
+                        <td colspan="5">No ESP Data received yet</td>
+                     </tr>`;
                 return;
             }
 
+            system.innerText = "System ONLINE";
+            system.className = "card blue";
+            totalBins.innerText = "Total Bins: 1";
+
             const d = res.data;
-            document.getElementById("totalBins").innerText = "Total Bins: " + d.total_bins;
-            document.getElementById("system").innerText = "System ONLINE";
 
             let status = "NORMAL";
             if (d.gas > 300 || d.level > 90) status = "CRITICAL";
             else if (d.gas > 150 || d.level > 70) status = "WARNING";
 
-            document.getElementById("tableBody").innerHTML = `
+            table.innerHTML = `
                 <tr>
                     <td>${d.bin_id}</td>
                     <td>${d.area}</td>
@@ -25,11 +38,9 @@ function fetchData() {
                     <td>${d.level}%</td>
                     <td>${status}</td>
                 </tr>`;
+        })
+        .catch(() => {
+            document.getElementById("system").innerText = "Connection Lost";
+            document.getElementById("system").className = "card red";
         });
-}
-
-function showNoData() {
-    document.getElementById("system").innerText = "Connection Lost";
-    document.getElementById("tableBody").innerHTML =
-        `<tr class="no-data"><td colspan="5">No ESP Data received yet</td></tr>`;
 }
